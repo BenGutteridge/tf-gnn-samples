0a0d310ac1b396f963eaaf552edac019e3fc1e50
BenGutteridge <benpgutteridge@gmail.com>
2022-12-24 13:58:03 +0000

    msg to check edge types

--- tasks/qm9_task.py
+++ tasks/qm9_task.py
@@ -78,9 +78,9 @@ class QM9_Task(Sparse_Graph_Task):
         self._loaded_data[DataFold.TRAIN] = self.__load_data(path.join("train.jsonl.gz"))
         # print('data:\n', self._loaded_data[DataFold.TRAIN])
         d = self._loaded_data[DataFold.TRAIN]
-        for d in range(10):
-            print(d)
-            for i, a in enumerate(d['adjacency_lists']):
+        for j in range(10):
+            print(j)
+            for i, a in enumerate(d[j]['adjacency_lists']):
                 print(i)
                 print('adjacency_lists:\n', a.shape)
         input('Press Enter to continue...')
